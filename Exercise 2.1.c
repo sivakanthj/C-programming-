e1e2849e2323bb14f24c56ac2fe6a28d0c63e5a6
sivakanthj <sivakanthj00@gmail.com>
2026-04-08 05:18:53 +0530

    Create Exercise 2.1.c

---
+++ Exercise 2.1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+    float a, b, c ;
+    printf("what is A in equation : ");
+    scanf("%f",&a);
+    printf("what is B in equation : ");
+    scanf("%f",&b);
+    printf("what is the constant in equation : ");
+    scanf("%f",&c);
+    printf("the equation is : %fx + %fy = %f",a,b,c);
+
+}
