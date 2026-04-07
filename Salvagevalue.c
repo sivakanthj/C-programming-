62268af928eeb8afe38d40dff6b7be1a2ab13c07
sivakanthj <sivakanthj00@gmail.com>
2026-04-08 05:03:00 +0530

    Create Salvagevalue.c

---
+++ Salvagevalue.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    double purchasePrice, depreciation, yearsOfService, salvageValue;
+    printf("Enter purchase price, annual depreciation and years of service:\n");
+    scanf("%lf %lf %lf", &purchasePrice, &depreciation, &yearsOfService);
+    salvageValue = purchasePrice - (depreciation * yearsOfService);
+    printf("Salvage Value = %.2lf\n", salvageValue);
+    return 0;
+}
