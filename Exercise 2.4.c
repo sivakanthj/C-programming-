2e5d58e840798a4ec2594b9f5b49745005e94a8a
sivakanthj <sivakanthj00@gmail.com>
2026-04-08 05:19:46 +0530

    Create Exercise 2.4.c

---
+++ Exercise 2.4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+    float a, b, c, x ;
+    printf("what is A : ");
+    scanf("%f",&a);
+    printf("what is B : ");
+    scanf("%f",&b);
+    printf("what is C : ");
+    scanf("%f",&c);
+    x = a/(b - c);
+    printf("value of x is : x = a/(b - c) ==> %f/(%f - %f) ==> %f",a,b,c,x);
+}
