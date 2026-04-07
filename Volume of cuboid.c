f474a2e2bb06a8b1f4d2e4da696df3714f223416
sivakanthj <sivakanthj00@gmail.com>
2026-04-08 05:12:47 +0530

    Create Volume of cuboid.c

---
+++ Volume of cuboid.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int h,l,b,a ;
+    printf("enter length of the cuboid : ");
+    scanf("%d",&l);
+    printf("enter breadth of the cuboid : ");
+    scanf("%d",&b);
+    printf("enter height of the cuboid : ");
+    scanf("%d",&h);
+    a=l*b*h ;
+    printf("total area of the cube is : %d",a);
+}
