ec0128a144d6bdba1a59b227e462f942664cab2b
sivakanthj <sivakanthj00@gmail.com>
2026-04-08 04:35:53 +0530

    Create Area of rectangle(with input).c

---
+++ Area of rectangle(with input).c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int l,b,a ;
+    printf("enter length of the rectangle: ");
+    scanf("%d",&l);
+    printf("enter breadth of the rectangle: ");
+    scanf("%d",&b);
+    a=l*b ;
+    printf("total area of the rectangle is : %d",a);
+}
